3392b3e6443fbb14829747be7d2e7bf49426708c
Namheon Baek <namhundred@naver.com>
2022-06-15 01:51:30 +0900

    feat: close button

--- main.js
+++ main.js
@@ -1,44 +1,61 @@
-// Modules to control application life and create native browser window
 const { app, BrowserWindow, ipcMain } = require("electron");
+const ipc = ipcMain;
 const path = require("path");
+
 function createWindow() {
-  // Create the browser window.
   const mainWindow = new BrowserWindow({
-    useContentSize: true,
+    width: 300,
+    height: 200,
     transparent: true,
     frame: false,
-    setMenu: null,
     webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true,
+      contextIsolation: false,
     },
   });
-  // and load the index.html of the app.
+
   mainWindow.loadFile("index.html");
   mainWindow.setAlwaysOnTop(true, "floating");
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  // 웹 페이지 로드 완료
+  mainWindow.webContents.on("did-finish-load", (evt) => {
+    // onWebcontentsValue 이벤트 송신
+    mainWindow.webContents.send("onWebcontentsValue", "on load...");
+  });
+  ipc.on("minimizeApp", () => {
+    mainWindow.minimize();
+  });
+
+  ipc.on("maximizeApp", () => {
+    if (mainWindow.isMaximized()) {
+      mainWindow.restore();
+    } else {
+      mainWindow.maximize();
+    }
+  });
+
+  ipc.on("closeApp", () => {
+    mainWindow.close();
+  });
 }
-ipcMain.handle("quit-app", () => {
-  app.quit();
-});
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+
 app.whenReady().then(() => {
   createWindow();
+
   app.on("activate", function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+
+  // onInputValue 이벤트 수신
+  ipcMain.on("onInputValue", (evt, payload) => {
+    console.log("on ipcMain event:: ", payload);
+
+    const computedPayload = payload + "(computed)";
+
+    // replyInputValue 송신 또는 응답
+    evt.reply("replyInputValue", computedPayload);
   });
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on("window-all-closed", function () {
   if (process.platform !== "darwin") app.quit();
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
